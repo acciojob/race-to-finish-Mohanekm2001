be802be4851ef830d5b16a52f9af3d94f9f11f52
Mohanekm2001 <mohan2001ekm@gmail.com>
2023-05-01 10:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
+  const promises = [
+        new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved"), Math.floor(Math.random() * 5000) + 1000)),
+        new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved"), Math.floor(Math.random() * 5000) + 1000)),
+        new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved"), Math.floor(Math.random() * 5000) + 1000)),
+        new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved"), Math.floor(Math.random() * 5000) + 1000)),
+        new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved"), Math.floor(Math.random() * 5000) + 1000))
+      ];
 
-
- const promises = [
-      new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), Math.floor(Math.random() * 5000) + 1000)),
-      new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.floor(Math.random() * 5000) + 1000)),
-      new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.floor(Math.random() * 5000) + 1000)),
-      new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.floor(Math.random() * 5000) + 1000)),
-      new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.floor(Math.random() * 5000) + 1000)),
-    ];
-
-    Promise.any(promises)
-      .then((result) => {
-        document.getElementById('output').textContent = result;
-      })
-      .catch((error) => {
-        console.error(error);
+      Promise.any(promises).then((result) => {
+        document.getElementById("output").innerText = result;
       });
